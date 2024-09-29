20c7e7a487e341ebf47198a3984e6b58ca656aae
Vaenq <art2sys048@gmail.com>
2024-09-29 18:19:34 +0300

    task solved

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Product {
+
+    private String name;
+    private Set<Product> children;
+    private Set<Product> parents;
+
+    public Product(String name) {
+        this.name = name;
+        this.children = new HashSet<>();
+        this.parents = new HashSet<>();
+    }
+
+    public boolean addProduct(Product p) {
+        Set<Product> visited = new HashSet<>();
+
+        if (p == this || hasCycleDFS(p, visited) || parents.contains(p)) {
+            return false;
+        }
+
+        p.parents.addAll(this.parents);
+        p.parents.add(this);
+        this.children.add(p);
+        return true;
+    }
+
+    private boolean hasCycleDFS(Product p, Set<Product> visited) {
+        if (visited.contains(this)) {
+            return true;
+        }
+
+        visited.add(this);
+
+        for (Product child : children) {
+            if (child == p || child.hasCycleDFS(p, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product p1 = new Product("тесто");
+        Product p2 = new Product("Мука");
+        Product p3 = new Product("Яйца");
+        Product p4 = new Product("Вода");
+        Product p5 = new Product("Пшеница");
+
+        System.out.println(p1.addProduct(p2));
+        System.out.println(p1.addProduct(p3));
+        System.out.println(p1.addProduct(p4));
+        System.out.println(p2.addProduct(p1));
+        System.out.println(p2.addProduct(p5));
+        System.out.println(p5.addProduct(p1));
+
     }
 }
